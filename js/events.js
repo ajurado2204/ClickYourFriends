a4b935c2f60ac8658b52aef14a823545cb3a0e78
Alejandra Jurado <ajurado2204@gmail.com>
2015-11-15 17:27:47 -0500

    finished game

--- js/events.js
+++ js/events.js
@@ -1,13 +1,35 @@
 var buttonImages = document.getElementsByTagName('button');
+var startButton = document.getElementById("start");
 var counter = 0;
 
-for(var i = 0; i < buttonImages.length; i++) {
-  buttonImages[i].addEventListener("click", function(event) {
-  		if(event.currentTarget.value == "unclicked"){
-  			counter = counter + 1;
-  			event.currentTarget.value = "clicked"
-  		}else{
-  			console.log(counter);
-  		}
-  });  
+startButton.addEventListener("click", beginGame);
+
+function beginGame(){
+	startButton.style.visibility = 'hidden';
+	setTimeout(disableFunc, 20000);
+
+	for(var i = 0; i < buttonImages.length; i++){
+  	buttonImages[i].addEventListener("click", clickCounter);  
+	}
+}
+
+function clickCounter(){
+	if(event.currentTarget.value == "unclicked"){
+				counter = counter + 1;
+				event.currentTarget.value = "clicked"
+	 		}else{
+	 			console.log(counter);		  		
+	 		}
+}
+
+function disableFunc(){
+	alert("You clicked " + counter + " of your friends!!");
+	startButton.style.visibility = 'visible';
+	counter = 0;
+
+	for(var i = 0; i < buttonImages.length; i++){
+		if(buttonImages[i].id != "start"){
+  		buttonImages[i].value = "unclicked";  
+  	}
+	}
 }
