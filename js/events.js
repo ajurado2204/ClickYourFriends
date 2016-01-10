8de42a418c3df8635ea271b9c3895c9eab1a1d1a
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-10 17:52:41 -0500

    Added modal popup instead of alert on games end

--- js/events.js
+++ js/events.js
@@ -1,38 +1,50 @@
-var buttonImages = document.getElementsByTagName('button');
-var startButton = document.getElementById("start");
-var counter = 0;
+$(document).ready(function(){
 
-startButton.addEventListener("click", beginGame);
+	// GLobal Variable
+	var buttonImages = document.getElementsByTagName('button');
+	var startButton = document.getElementById("start");
+	var counter = 0;
 
-function beginGame(){
-	startButton.style.visibility = 'hidden';
-	setTimeout(disableFunc, 20000);
+	//Begins Game on Click of Start Button
 
-	for(var i = 0; i < buttonImages.length; i++){
-		buttonImages[i].disabled = false;
-  		buttonImages[i].addEventListener("click", clickCounter);  
-	}
-}
+	startButton.addEventListener("click", beginGame);
 
-function clickCounter(){
-	if(event.currentTarget.value == "unclicked"){
-				counter = counter + 1;
-				event.currentTarget.value = "clicked"
-	 		}else{
-	 			console.log(counter);		  		
-	 		}
-}
+	function beginGame(){
+		startButton.style.visibility = 'hidden';
+		setTimeout(function(){disableFunc();}, 20000);
 
-function disableFunc(){
-	alert("You clicked " + counter + " of your friends!!");
-	startButton.style.visibility = 'visible';
-	counter = 0;
+		for(var i = 0; i < buttonImages.length; i++){
+			buttonImages[i].disabled = false;
+	  	buttonImages[i].addEventListener("click", clickCounter);  
+		};
+	};
 
-	for(var i = 0; i < buttonImages.length; i++){
-		if(buttonImages[i].id != "start"){
-  			buttonImages[i].value = "unclicked";  
-  			buttonImages[i].disabled = "disabled";
-  		}
-	}
+	//Function to Counts Number of Clicks
 
-}
+	function clickCounter(){
+		if(event.currentTarget.value == "unclicked"){
+			counter = counter + 1;
+			event.currentTarget.value = "clicked"
+		 }else{
+		 		console.log(counter);		  		
+		 	};
+	};
+
+	//Function to Show Results and to reset Game
+
+	function disableFunc(){
+		$('#myModal').modal('show');
+		//alert("You clicked " + counter + " of your friends!!");
+		startButton.style.visibility = 'visible';
+		counter = 0;
+
+		for(var i = 0; i < buttonImages.length; i++){
+			if(buttonImages[i].id != "start"){
+	  		buttonImages[i].value = "unclicked";  
+	  		buttonImages[i].disabled = "disabled";
+	  	};
+		};
+	};
+
+
+});
